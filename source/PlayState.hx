9485c31d66038f54b0d0a074cdd15ee99fa67048
DonniArdiansyah <97490314+DonniArdiansyah@users.noreply.github.com>
2022-06-10 13:52:27 +0700

    update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -874,22 +874,6 @@ class PlayState extends MusicBeatState
 				// evilTrail.changeGraphic()
 				add(evilTrail);
 				// evilTrail.scrollFactor.set(1.1, 1.1);
-			case 'edd-house':
-				{
-						defaultCamZoom = 0.9;
-						curStage = 'edd-house';
-						var bg:FlxSprite = new FlxSprite(-600, -200).loadGraphic(Paths.image('edd-house'));
-						bg.antialiasing = true;
-						bg.scrollFactor.set(0.9, 0.9);
-						bg.active = false;
-						add(bg);
-				}
-
-
-				boyfriend.x += 200;
-				boyfriend.y += 220;
-				gf.x += 180;
-				gf.y += 300;
 		}
 
 		if (FlxG.save.data.char)
